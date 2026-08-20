d644119aea5b37c57b635260f268bf6f2cbdc7de
Ashu Prakash <aspksh@gmail.com>
2026-08-20 22:52:01 +0530

    Update faq_streamlit_app.py

--- faq_streamlit_app.py
+++ faq_streamlit_app.py
@@ -52,7 +52,7 @@ st.title("FAQ Chatbot")
 if "messages" not in st.session_state:
     st.session_state.messages = []
     
-for "message" in st.session_state.messages:
+for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
         
