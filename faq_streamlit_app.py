fb94dd50acaa4b15edfdacecdba07947de063298
Ashu Prakash <aspksh@gmail.com>
2026-08-20 22:46:38 +0530

    Update faq_streamlit_app.py

--- faq_streamlit_app.py
+++ faq_streamlit_app.py
@@ -49,22 +49,36 @@ def get_answer(user_query, threshold = 0.4):
 
 
 st.title("FAQ Chatbot")
-
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+    
+for "messages" in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message[content])
+        
 user_input = st.chat_input("Ask your question...")
 
 if user_input:
 
-    st.chat_message("user").write(user_input)
+    st.session_state.messages.append({
+        "role" : "user",
+        "content" : "user_input"
+    })
 
+    with st.chat_message("user"):
+        st.write(user_input)
+        
     result = get_answer(user_input)
 
     with st.chat_message("assistant"):
 
         if result['found']:
-
             st.write(result['answer'])
-
         else:
-
             st.write(result['answer'])
             st.write(f"(Confidence was only {result['confidence']:.3f})")
+    
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": answer
+    })
